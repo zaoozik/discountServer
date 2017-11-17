32000992f450653a0e593c39d95a8e70d3fd81b3
k_igumenov <bruce1thomas1wayne@gmail.com>
2017-11-17 18:07:04 +0300

    пятничный веселый комит
    Добавлены ajax фильтры и промотки списка карт. Выглядит более менее. JS для страницы с картами перенесен в отдельный файл

--- cards/views.py
+++ cards/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.core.exceptions import ObjectDoesNotExist
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpRequest, HttpResponse
+from django.db.models import Q
 from cards.models import Card
 from orgs.models import Org
 from cards.serializers import CardSerializer
@@ -14,6 +15,14 @@ from .forms import CardForm
 import json
 
 # Create your views here.
+
+def is_digit(a):
+    try:
+        b = int(a)
+        return True
+    except:
+        return False
+
 
 
 @login_required(login_url='../login/')
@@ -31,18 +40,53 @@ def listCards(request):
             cards = Card.objects.filter(org__exact=user.org, deleted__exact='n')
         if "search" in request.GET:
             phrase = request.GET["search"]
-            try:
-                a = int(phrase)
-                is_digit = True
-            except:
-                is_digit = False
-            if is_digit:
+            if is_digit(phrase):
                 cards = cards.filter(code__exact=phrase)
             else:
                 cards = cards.filter(holder_name__contains=phrase)
         if cards is not None:
             response = {"cards": cards, "form": form}
             return HttpResponse(template.render(response, request))
+    if request.method == "POST":
+        user = UserCustom.objects.get(user_id__exact=request.user.pk)
+        q = Q(org_id__exact=user.org.pk)
+        data = None
+        selection_parameters = None
+        post = request.POST
+        if "cmd" in post:
+            if post["cmd"] == "update":
+                if "data" in post:
+                    data = json.loads(post["data"])
+                if "selection_parameters" in post:
+                    selection_parameters = json.loads(post["selection_parameters"])
+                if selection_parameters is not None:
+                    if selection_parameters["deleted"] == "n" or selection_parameters["deleted"] == "":
+                        q &= Q(deleted__exact='n')
+                    if is_digit(selection_parameters["search"]):
+                        q &= Q(code__startswith=selection_parameters["search"])
+                    elif selection_parameters["search"] != '':
+                        q &= Q(holder_name__contains=selection_parameters["search"])
+                cards = Card.objects.filter(q)
+                total = len(cards)
+                if data["count"] > total:
+                    data["count"] = total;
+                cards = cards.order_by(selection_parameters["sort"]).all()[data["start"]:data["start"]+data["count"]]
+                resp_cards=[]
+                for card in cards:
+                    resp_cards.append(
+                        {
+                            "code": card.code,
+                            'holder_name': card.holder_name,
+                            'accumulation': card.accumulation,
+                            'deleted': card.deleted,
+                        }
+                    )
+
+                response = {"result": "ok", "data": resp_cards, "total": total}
+                return HttpResponse(json.dumps(response), content_type="application/json")
+
+
+
 
 
 @login_required(login_url='./login/')
---
+++ discountServer/static/js/cards.js
@@ -0,0 +1,330 @@
+    var buffer= '';
+    var start = 0;
+    var count = 10;
+    var total =3;
+    var elems_end = false;
+    var selection_parameters ={
+        "sort": "code",
+        "search": "",
+        "deleted": "n",
+    };
+
+    // Устанавливаем строку поиска
+    $('#search').change(function(){
+        selection_parameters["search"] = $(this).val();
+    });
+
+     // Устанавливаем порядок сортировки
+    $('#sort').change(function(){
+        selection_parameters["sort"] = $(this).val();
+    });
+
+    //Включая/выключая удаленных
+    $('#showDeleted').change(function(){
+        if ($(this).is(':checked')){
+            selection_parameters["deleted"] = "y";
+            } else {
+            selection_parameters["deleted"] = "n";
+            }
+    });
+
+    $('html').keydown(function(e){ //отлавливаем нажатие клавиш
+
+        if (e.keyCode == 13) {
+            if (! $('#addCardModal').hasClass("show"))
+            {
+            $(document).click();
+            $('#addCardButton').click();}
+
+            $('#addCardCode').val(buffer);
+            $("#one").on("input", function() {
+
+                setTimeout(function(){
+                $("#addCardCode").focus();
+                    }, 2);
+
+});
+            buffer= '';
+            }
+    if (is_numeric(String.fromCharCode(e.keyCode))){
+            buffer+=String.fromCharCode(e.keyCode);}
+});
+
+function saveCard(){
+    var code= $("#id_code").val();
+    var holder_name = $("#id_holder_name").val();
+    var card_data = {"code": code,
+                        "holder_name": holder_name}
+    var cmd = "save";
+
+    $.ajax({
+        url: '/cards/maintenance/',
+        type: "POST",
+        data: {
+          "cmd": cmd,
+          "data": JSON.stringify(card_data)
+        },
+        dataType: "json",
+        success: function (data) {
+          if (data.result) {
+            if (data.result == "ok"){
+                window.location.href = window.location.href;
+            }
+            else
+            {
+               alert("Ошибка!");
+            }
+         }
+        }
+      });
+
+}
+
+function clearForm(){
+          $('#id_code').val('');
+           $('#id_holder_name').val('');
+            $('#id_accumulation').val(0);
+            $('#id_bonus').val(0);
+            $('#id_discount').val(0);
+}
+
+function getCard(card_code)
+{
+    var card_data = {"code": card_code}
+    var cmd = "get";
+    var cData;
+
+    $.ajax({
+        url: '/cards/maintenance/',
+        type: "POST",
+        data: {
+          "cmd": cmd,
+          "data": JSON.stringify(card_data)
+        },
+        dataType: "json",
+        success: function (response) {
+          if (response.result) {
+            if (response.result == "ok"){
+                cData = response.data;
+                $('#id_code').val(cData.code);
+                $('#id_holder_name').val(cData.holder_name);
+                $('#id_accumulation').val(cData.accumulation);
+                $('#id_bonus').val(cData.bonus);
+                $('#id_discount').val(cData.discount);
+
+            }
+            else
+            {
+               alert(data.msg);
+            }
+         }
+        }
+      });
+}
+
+function deleteCard(){
+
+    var select = $('.cardcode:checked');
+    if (select.length == 0)
+    {return;}
+
+    var card_codes = [];
+
+    $.each(select, function(i, item){
+     card_codes[card_codes.length] = item.attributes["id"].value;
+    }
+    );
+
+
+    var cmd = "delete";
+
+    $.ajax({
+        url: '/cards/maintenance/',
+        type: "POST",
+        data: {
+          "cmd": cmd,
+          "data": JSON.stringify(card_codes)
+        },
+        dataType: "json",
+        success: function (data) {
+          if (data.result) {
+            if (data.result == "ok"){
+                window.location.href = '../cards/';
+            }
+            else
+            {
+               alert("Ошибка!");
+            }
+         }
+        }
+      });
+
+}
+$(document).ready(function(){
+        dataUpdate();
+
+        //$('#data').css('margin-top', '200px');
+});
+
+
+function dataUpdate(){
+    var cmd = "update";
+    start= 0;
+    count= 10;
+    var data = {
+            "start": start,
+            "count": count,
+        }
+
+     $.ajax({
+        url: '/cards/',
+        type: "POST",
+        data: {
+          "cmd": cmd,
+          "data": JSON.stringify(data),
+          "selection_parameters": JSON.stringify(selection_parameters)
+        },
+        dataType: "json",
+        success: function (data) {
+          if (data.result) {
+            if (data.result == "ok"){
+                $('#tbody').empty();
+                total = data.total;
+                if (total < count){
+                count = total;
+                }
+                $('#total').text(start+count + ' из ' + total);
+
+                $.each(data.data, function(index, value){
+                    var tr='';
+                    if (value["deleted"] == 'y')
+                    {
+                       tr = '<tr style="color: red; text-decoration:line-through;">'
+                    }
+                    else
+                    {
+                        tr = '<tr>'
+                    }
+                    /*var html = document.createElement('tr');
+                     var td = document.createElement('td');
+                     $(td).add('index').prop('type','checkbox');
+                     $(html).add(td);
+                     $(html).add('td').text(value["code"]);
+                     $(html).add('td').text(value["accumulation"]);*/
+                     var html = tr+
+                         '<td><input class="cardcode" type="checkbox" id="' + value["code"] + '"></td> \
+                         <td>' + value["code"] + '</td> \
+                         <td>' + value["accumulation"] + '</td> \
+                         <td>' + value["holder_name"] + '</td> \
+                         <td><button data-toggle="modal" data-target="#CardModal" type="button" onclick="getCard(';
+                         html += "'" + value["code"] + "'";
+                          html += ');" class="fa fa-eye" aria-hidden="true" title="Просмотр"></button></td> \
+                            </tr> ';
+                     //alert(html);
+                    $('#tbody').append( html);
+                });
+                start = start + count;
+                    if (start > total)
+                    {
+                        start = start - count;
+                        elems_end = true;
+                    }
+            }
+            else
+            {
+               alert("Ошибка!");
+            }
+         }
+        }
+      });
+}
+
+function dataAdd(){
+    if (elems_end)
+    {
+        return;
+    }
+    var cmd = "update";
+    if (start+count > total)
+    {
+        count = total - start;
+        }
+
+    var data = {
+            "start": start,
+            "count": count,
+        }
+
+     $.ajax({
+        url: '/cards/',
+        type: "POST",
+        data: {
+          "cmd": cmd,
+          "data": JSON.stringify(data),
+          "selection_parameters": JSON.stringify(selection_parameters)
+        },
+        dataType: "json",
+        success: function (data) {
+          if (data.result) {
+            if (data.result == "ok"){
+                total = data.total;
+                $('#total').text(start+count + ' из ' + total);
+
+                $.each(data.data, function(index, value){
+                    var tr='';
+                    if (value["deleted"] == 'y')
+                    {
+                       tr = '<tr style="color: red; text-decoration:line-through;">'
+                    }
+                    else
+                    {
+                        tr = '<tr>'
+                    }
+
+                    var html = tr+
+                         '<td><input class="cardcode" type="checkbox" id="' + value["code"] + '"></td> \
+                         <td>' + value["code"] + '</td> \
+                         <td>' + value["accumulation"] + '</td> \
+                         <td>' + value["holder_name"] + '</td> \
+                         <td><button data-toggle="modal" data-target="#CardModal" type="button" onclick="getCard(';
+                         html += "'" + value["code"] + "'";
+                          html += ');" class="fa fa-eye" aria-hidden="true" title="Просмотр"></button></td> \
+                            </tr> ';
+                     //alert(html);
+                    $('#tbody').append(html);
+                });
+                start = start + count;
+                    if (start > total)
+                    {
+                        start = start - count;
+                        elems_end = true;
+                    }
+            }
+            else
+            {
+               alert("Ошибка!");
+            }
+         }
+        }
+      });
+}
+
+function clearFilter(){
+    selection_parameters ={
+        "sort": "code",
+        "search": "",
+        "deleted": "n",
+    };
+    $('#showDeleted').prop('checked', false);
+    start = 0;
+    count = 10;
+    dataUpdate();
+}
+
+$(window).scroll(function()
+{
+     if  ($(window).scrollTop() +$(window).height() >= $(document).height())
+     {
+          dataAdd();
+     }
+});
